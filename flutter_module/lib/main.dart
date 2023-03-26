298e8cda1f7307dfcb975fd3e88f9f82c1330630
zhengzeqin <zeqinjie@qq.com>
2023-03-26 22:29:32 +0800

    update：attach 模式

--- flutter_module/lib/main.dart
+++ flutter_module/lib/main.dart
@@ -5,8 +5,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_module/client/client.dart';
 import 'package:flutter_module/page/home_page.dart';
+import 'package:flutter_module/tool/log.dart';
 
-void main() {
+void main(List<String> args) {
+  TWLog('原生传的 args ==> $args');
   Client.instance.init();
   runApp(const HomePage(color: Colors.blue));
 }
